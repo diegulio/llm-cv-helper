898c467bbea2331911da1d6a660fcbec0d72e62d
diegulio <diego.machado@usach.cl>
2023-07-21 15:46:56 -0400

    add uploaded file

--- src/app.py
+++ src/app.py
@@ -5,3 +5,13 @@ from langchain.memory import ConversationBufferMemory
 from langchain.schema import AIMessage, HumanMessage
 
 from src.utils import get_documents, split_documents, store_vectors
+
+### APP ###
+def app(project_id, model_name, chunk_size, chunk_overlap, persist_directory):
+    st.title("📩 CV Reviewer")
+
+    with st.sidebar:
+        st.header("Choose your avatar:")
+        user_avatar = st.radio("Avatar", ["👨‍⚕️", "👩‍⚕️", "👽"])
+        st.header("Upload yout CV here")
+        uploaded_file = st.file_uploader("Upload a CV", type="pdf")
